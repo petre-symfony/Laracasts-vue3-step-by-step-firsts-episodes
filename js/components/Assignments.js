1a8dfc19b972aaee48995a03f1e49f632b79ce46
petrero <petrero@gmail.com>
2024-01-06 19:41:54 +0200

    8.4 Okay, so now think about it when the user types in here and when they press the submit button, we should then catch that and add the new assignment to the list. Okay, here's how we do that. The first step is I'm going to listen for a submit events now a handful of episodes ago you learned about using V on and I think we used the click event as an example and that works and also don't forget to the Alias of @ symbol this listens for a click event on the form in our case though. I actually want to listen for when the form is submitted. So why don't we say when it's submitted call a method named add and we'll do that here methods add and for now, let's just alert hi there to make sure it's working.
     Okay, come back refresh submit the form and yes, I get an alert here. But I want you to pay attention right up here. When I click on it. Did you see that the page refreshed? And of course it did we submitted the form.

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -8,7 +8,7 @@ export default {
       <assignments-list :assignments="filters.inProgress" title="In Progress"></assignments-list>
       <assignments-list :assignments="filters.completed" title="Completed"></assignments-list>
       
-      <form >
+      <form @submit="add">
         <div class="border border-gray-600 text-black">
           <input placeholder="New assignment..." class="p-2">
           <button type="submit" class="bg-white p-2 border-l">Add</button>
@@ -31,6 +31,11 @@ export default {
         inProgress: this.assignments.filter(assignment => !assignment.complete),
         completed: this.assignments.filter(assignment => assignment.complete)
       }
+    }
+  },
+  methods: {
+    add() {
+      alert('hi there')
     }
   }
 }
