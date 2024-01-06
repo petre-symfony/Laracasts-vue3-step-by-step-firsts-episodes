1daa70ed8b68b052dd33d050bee1c63c928e5097
petrero <petrero@gmail.com>
2024-01-06 15:13:14 +0200

    6.4 But if we instead change it False that means do not apply these classes as you see there cool.

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -2,7 +2,7 @@ export default {
   template: `
     <button 
         :class="{
-          'bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled: cursor-not-allowed': true
+          'bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled: cursor-not-allowed': false
         }" 
         :disabled="processing"
     >
