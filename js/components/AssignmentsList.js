a38dab4ab65b3e19c231511465b0e4da08d944ea
petrero <petrero@gmail.com>
2024-01-06 16:49:20 +0200

    7.6 Make an item component in AsignmentsList component

--- js/components/AssignmentsList.js
+++ js/components/AssignmentsList.js
@@ -1,14 +1,18 @@
+import Assignment from "./Assignment.js";
+
 export default {
+  components: {
+    Assignment
+  },
   template: `<section v-show="assignments.length" class="mt-8">
       <h2 class="font-bold mb-5">{{ title }}</h2>
     
       <ul>
-        <li v-for="assignment in assignments" :key="assignment.id">
-          <label >
-            {{ assignment.name }}
-            <input type="checkbox" v-model="assignment.complete">
-          </label>
-        </li>
+        <assignment 
+          v-for="assignment in assignments" 
+          :key="assignment.id"
+          :assignment="assignment"
+        >
       </ul>
     </section>
   `,
