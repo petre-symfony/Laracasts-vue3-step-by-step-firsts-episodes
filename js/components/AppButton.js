cc6a7b217d02d95c5eae0d37239ae341c2211e66
petrero <petrero@gmail.com>
2024-01-06 15:10:01 +0200

    6.3 Now, let's use it and it's very simple and I'll show you a little trick when you're setting class bindings. You can use a string you could use an array or you could use an object. Let's take the object approach. So I will cut all of this out replace it with an object. And now the key of the object will be the class names so I could paste all of those in there and then the value would be a Boolean and if that Boolean is true, we add the classes to the elements if the Boolean is false, we do not so for example, if I set true here have a look if I come back and refresh I should still see my great button

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,6 +1,11 @@
 export default {
   template: `
-    <button class="bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled:cursor-not-allowed" :disabled="processing">
+    <button 
+        :class="{
+          'bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled: cursor-not-allowed': true
+        }" 
+        :disabled="processing"
+    >
       <slot></slot>
     </button>
   `,
