b9a7ffe4a454f2f3c5a175dd8c7a9bb571a63c8c
petrero <petrero@gmail.com>
2024-01-06 15:48:22 +0200

    7.2 Make a template for all of our assignment in App.js

--- js/components/App.js
+++ js/components/App.js
@@ -1,7 +1,28 @@
-import AppButton from "./AppButton.js";
-
 export default {
-  components: {
-    'app-button': AppButton
-  }
-};
+  template: `<section v-show="inProgressAssignments.length">
+      <h2 class="font-bold mb-5">In Progress</h2>
+    
+      <ul>
+        <li v-for="assignment in inProgressAssignments" :key="assignment.id">
+        <label >
+          {{ assignment.name }}
+          <input type="checkbox" v-model="assignment.complete">
+        </label>
+      </li>
+    </ul>
+    </section>
+    
+    <section v-show="completedAssignments.length" class="mt-8">
+      <h2 class="font-bold mb-5">Completed</h2>
+    
+      <ul>
+        <li v-for="assignment in completedAssignments" :key="assignment.id">
+        <label >
+          {{ assignment.name }}
+          <input type="checkbox" v-model="assignment.complete">
+        </label>
+      </li>
+    </ul>
+  </section>
+  `
+}
