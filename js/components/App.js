a439e5af269569d0f00403378f38bad7f6ff193c
petrero <petrero@gmail.com>
2024-01-06 16:07:59 +0200

    7.4 Componetize the list section. Fill the Assignments component

--- js/components/App.js
+++ js/components/App.js
@@ -1,45 +1,8 @@
+import Assignments from "./Assignments.js";
+
 export default {
-  template: `<section v-show="inProgressAssignments.length">
-      <h2 class="font-bold mb-5">In Progress</h2>
-    
-      <ul>
-        <li v-for="assignment in inProgressAssignments" :key="assignment.id">
-        <label >
-          {{ assignment.name }}
-          <input type="checkbox" v-model="assignment.complete">
-        </label>
-      </li>
-    </ul>
-    </section>
-    
-    <section v-show="completedAssignments.length" class="mt-8">
-      <h2 class="font-bold mb-5">Completed</h2>
-    
-      <ul>
-        <li v-for="assignment in completedAssignments" :key="assignment.id">
-        <label >
-          {{ assignment.name }}
-          <input type="checkbox" v-model="assignment.complete">
-        </label>
-      </li>
-    </ul>
-  </section>
-  `,
-  data() {
-    return {
-      assignments: [
-        {name: 'Finish project', complete: false, id: 1},
-        {name: 'Read chapter 4', complete: false, id: 2},
-        {name: 'Turn in homework', complete: false, id: 3}
-      ]
-    }
+  components: {
+    Assignments
   },
-  computed: {
-    inProgressAssignments() {
-      return this.assignments.filter(assignment => !assignment.complete)
-    },
-    completedAssignments() {
-      return this.assignments.filter(assignment => assignment.complete)
-    }
-  }
+  template: `<Assignments />`
 }
