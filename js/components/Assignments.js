3aeb0874169e04694dceb8a907cef3a0961c0ae2
petrero <petrero@gmail.com>
2024-01-06 16:46:23 +0200

    7.5 Replace sections with assignments-list component in App

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -1,29 +1,11 @@
+import AssignmentsList from "./AssignmentsList.js";
 export default {
-  template: `<section v-show="inProgressAssignments.length">
-      <h2 class="font-bold mb-5">In Progress</h2>
-    
-      <ul>
-        <li v-for="assignment in inProgressAssignments" :key="assignment.id">
-        <label >
-          {{ assignment.name }}
-          <input type="checkbox" v-model="assignment.complete">
-        </label>
-      </li>
-    </ul>
-    </section>
-    
-    <section v-show="completedAssignments.length" class="mt-8">
-      <h2 class="font-bold mb-5">Completed</h2>
-    
-      <ul>
-        <li v-for="assignment in completedAssignments" :key="assignment.id">
-        <label >
-          {{ assignment.name }}
-          <input type="checkbox" v-model="assignment.complete">
-        </label>
-      </li>
-    </ul>
-  </section>
+  components: {
+    AssignmentsList
+  },
+  template: `
+    <assignments-list :assignments="inProgressAssignments" title="In Progress"></assignments-list>
+    <assignments-list :assignments="completedAssignments" title="Completed"></assignments-list>
   `,
   data() {
     return {
---
+++ js/components/AssignmentsList.js
@@ -0,0 +1,19 @@
+export default {
+  template: `<section v-show="assignments.length" class="mt-8">
+      <h2 class="font-bold mb-5">{{ title }}</h2>
+    
+      <ul>
+        <li v-for="assignment in assignments" :key="assignment.id">
+          <label >
+            {{ assignment.name }}
+            <input type="checkbox" v-model="assignment.complete">
+          </label>
+        </li>
+      </ul>
+    </section>
+  `,
+  props: {
+    assignments: Array,
+    title: String
+  }
+}
