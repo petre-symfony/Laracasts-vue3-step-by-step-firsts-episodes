7ad1458355ea5d0363adb6fd1852ce8c6a5207b8
petrero <petrero@gmail.com>
2024-01-06 15:24:36 +0200

    6.5 So what we could do is set a base set of classes and let's hide the sidebar.
     Which would be a border rounded some padding, but then the colors those will be unique to the style or the type like this.
    
     Apply a great background if the type is how about muted?
    
     All right. Let's do a couple more. We'll do primary secondary and muted.
    
     So we'll do primary secondary.
    
     For primary will say maybe if it's blue something like that.
    
     Secondary, you know, whatever you want purple Maybe.
    
     Alright, let's have a look. And yeah, it's it's pastel purple.
    
     Maybe we're building a My Little Pony site, but you get the idea we can now configure the style of the button based upon an incoming prop value.
    
     So let's try out all variations. We have secondary we have muted which should be the default gray and we have primary which is again a My Little Pony shade of blue or of course, if we don't pass in any type, it will still default to Blue.

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -2,7 +2,10 @@ export default {
   template: `
     <button 
         :class="{
-          'bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled: cursor-not-allowed': false
+          'border rounded px-5 py-2 disabled: cursor-not-allowed': true,
+          'bg-gray-200 hover:bg-gray-400': type==='muted',
+          'bg-blue-200 hover:bg-blue-400': type==='primary',
+          'bg-purple-200 hover:bg-purple-400': type==='secondary'
         }" 
         :disabled="processing"
     >
