2b8257d16c4acc21f2a23c098abc5b7c848d954c
petrero <petrero@gmail.com>
2024-01-08 09:51:46 +0200

    15.2 So we have these little cards here. But now as you can imagine you'll probably want to reuse these cards.
     So, let's see right now what you would have to do.
    
     Let's wrap this within a div like so and yeah, maybe down here. We will want another card. So what you'll do is you'll return to some place where you already used it and you'll see. Okay. Looks like we have these Styles and it looks like there's a heading to that is bold with margin.
    
     And yeah, you'll just you know paste it on here.Heading to font bold.
    
     Hello world and yeah, this will do the trick. So if I come back and give it a refresh so yeah, it works but it's not ideal. And of course as the application grows there becomes greater opportunity for these things to become out of sync

--- js/components/App.js
+++ js/components/App.js
@@ -4,5 +4,13 @@ export default {
   components: {
     Assignments
   },
-  template: `<Assignments />`
+  template: `
+    <div>
+      <Assignments />
+      
+      <div class="bg-gray-700 p-4 border border-gray-400 rounded">
+        <h2 class="font-bold">Hello</h2>
+      </div>
+    </div>
+  `
 }
